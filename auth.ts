1f8d29b9e5dcff5b7e0b727cb22ed14d94fdec1f
Hashir <hashir0601.be21@chitkara.edu.in>
2024-01-07 23:00:47 +0530

    created token functunality for user logging in with credentials

--- auth.ts
+++ auth.ts
@@ -28,13 +28,17 @@ export const {
     },
   },
   callbacks: {
-    // async signIn({ user }) {
-    //   const existingUser = await getUserById(user.id);
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
 
-    //   if (!existingUser || !existingUser.emailVerified) return false;
+      const existingUser = await getUserById(user.id);
 
-    //   return true;
-    // },
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: add 2FA check
+
+      return true;
+    },
 
     async session({ token, session }) {
       if (token.sub && session.user) {
